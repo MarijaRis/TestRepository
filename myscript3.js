625a35db9928d78f1096d48bcc9e9fdfa68ec637
MarijaRis <marija_ristovska@yahoo.com>
2018-11-25 23:50:05 +0100

    initialized repo

---
+++ myscript3.js
@@ -0,0 +1,46 @@
+function random (){
+  var i=0;
+  var random= 3;
+ //return random;
+} while(i<10){
+
+ log();
+}
+
+
+function getBiggest(){
+ var brojac = 0;
+ var najgolem = 0;
+ while (brojac < 10){
+     var randomNum=Math.random();
+     if (randomNum > najgolem){
+         najgolem = randomNum;
+     }
+brojac++;
+ }
+}
+return najgolem; 
+
+ //var result = getBigest();
+
+ function sumSquare(){
+var brojac=101 
+ }
+
+ function myFunc(x, y){
+     var myVar = x * y;
+     console.log(myVar);
+     return myVar;
+    
+ }
+
+ var greeting = "Dobar den";
+ var name = "Marija";
+  var welcomeMessage = greeting + name; {
+      document.write(welcomeMessage);
+  }
+  
+
+function testFunction(a, b){
+    var test = a * b;
+}
